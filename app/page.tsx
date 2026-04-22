f8bb7c8118b6f4ca3fe3f74ce04591869e24ed3d
navraj-singh <issinghking538@gmail.com>
2026-04-22 23:17:43 +0530

    Add main entry page

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: 'black', color: '#00ff00', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontFamily: 'monospace' }}>
+      <h1>Cyberpunk OSINT Hub - System Online</h1>
+    </div>
+  );
+}
